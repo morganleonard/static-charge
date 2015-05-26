13bba05ee9371b8bc672f19f29447bbce2208849
Morgan Leonard <morganleonardpublic@gmail.com>
2015-05-26 15:46:26 -0700

    Working with two posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(request, response, next) {
+  response.render('index', { title: "Morgl's Blog" });
 });
+
+
+//create routes for each page in posts directory
+fs.readdir(__dirname + '/../posts', function(error, directoryContents) {
+	if (error){
+		throw new Error(error);
+	}
+
+	directoryContents.forEach(function(postFileName) {
+		var postName = postFileName.replace('.jade', '');
+		router.get('/' + postName, function(request, response) {
+			response.render('../posts/' + postFileName, {});
+		})
+	})
+})
 
 module.exports = router;
