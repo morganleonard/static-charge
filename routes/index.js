bd15dcd78fc55fc120e5834b8a2e78f5986d8d0b
Morgan Leonard <morganleonardpublic@gmail.com>
2015-05-26 16:44:17 -0700

    changed to use .md and updated routing process

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var marked = require('marked');
 
-/* GET home page. */
-router.get('/', function(request, response, next) {
-  response.render('index', { title: "Morgl's Blog" });
-});
-
+// /* GET home page. */
+// router.get('/', function(request, response, next) {
+//   response.render('index', { title: "Morgl's Blog" });
+// });
 
 //create routes for each page in posts directory
-fs.readdir(__dirname + '/../posts', function(error, directoryContents) {
+var postsDir = __dirname + "/../posts/";
+fs.readdir(postsDir, function(error, directoryContents) {
 	if (error){
 		throw new Error(error);
 	}
 
-	directoryContents.forEach(function(postFileName) {
-		var postName = postFileName.replace('.jade', '');
-		router.get('/' + postName, function(request, response) {
-			response.render('../posts/' + postFileName, {});
+	var posts = directoryContents.map(function(filename) {
+		var postName = filename.replace('.md', '');
+		var contents = fs.readFileSync(postsDir + filename, {encoding: 'utf-8'});
+		return {postName: postName, contents: marked(contents)};
+	});
+
+	router.get('/', function(request, response) {
+		response.render('index', {posts:posts, title: 'all posts'})
+	});
+
+	posts.forEach(function(post) {
+		router.get('/' + post.postName, function(request, response) {
+			response.render('post', {postContents: post.contents})
 		})
 	})
 })
+
+
+// 	directoryContents.forEach(function(postFileName) {
+// 		var postName = postFileName.replace('.md', '');
+// 		fs.readFile(postsDir + postFileName, {encoding: 'utf-8'}, function(error, fileContents) {
+// 			if(error) {
+// 				throw new Error(error);
+// 			}
+
+// 			var renderedPost = marked(fileContents);
+		
+// 			router.get('/' + postName, function(request, response) {
+// 				response.render('post', {postContents: renderedPost});
+// 			})
+
+// 		})
+// 	})
+// })
 
 module.exports = router;
